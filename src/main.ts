ae31885f56b2e46a9471afe213d991fef59d6d55
Sebaledex <91289240+Sebaledex@users.noreply.github.com>
2024-09-12 23:16:18 -0300

    Base gateway 0.5
    Se agregaro conexion rabbit y user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+
+  const options = new DocumentBuilder()
+  .setTitle('WebMovil API 2024')
+  .setDescription('App WebMovil')
+  .setVersion('2.0.0')
+  .addBearerAuth()
+  .build();
+
+const document = SwaggerModule.createDocument(app, options);
+
+SwaggerModule.setup('/api/docs', app, document, {
+  swaggerOptions: {
+    filter: true,
+  },
+});
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
