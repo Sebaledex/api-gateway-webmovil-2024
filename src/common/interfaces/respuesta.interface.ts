a6c2bfe64cb415ab0f05a7734394afb2fdcbd188
Sebastian <sebastian.molina01@alumnos.ucn.cl>
2024-11-25 22:59:10 -0300

    se actualiza las respuestas

--- src/common/interfaces/respuesta.interface.ts
+++ src/common/interfaces/respuesta.interface.ts
@@ -1,12 +1,17 @@
 
 export interface IAnswer {
-    numeroPregunta: number; // Número de la pregunta dentro del cuestionario
+    numero: number; // Número de la pregunta dentro del cuestionario
     respuestaSeleccionada: string; // Respuesta seleccionada por el usuario
-   
-}
-
-export interface IResponse extends Document {
+  }
+  
+  export interface IResponse extends Document {
     cuestionario_id: string; // ID del cuestionario al que pertenece la respuesta
     user_id: string; // ID del usuario que respondió el cuestionario
     respuestas: IAnswer[]; // Array de respuestas para cada pregunta
-}
+    patente: string; // Patente del vehículo utilizado al responder
+    fecha_respuesta: Date; // Fecha y hora en que se respondió el cuestionario
+    geolocalizacion: {
+      latitud: number; // Latitud de la ubicación
+      longitud: number; // Longitud de la ubicación
+    }; // Geolocalización al momento de responder
+  }
--- src/respuesta/dto/respuesta.dto.ts
+++ src/respuesta/dto/respuesta.dto.ts
@@ -1,10 +1,16 @@
 export class RespuestaDTO {
-    readonly user_id: string; // ID del usuario que responde el cuestionario
-    readonly cuestionario_id: string; // ID del cuestionario respondido
-    readonly respuestas: RespuestaPreguntaDTO[]; // Array de respuestas de las preguntas
-  }
-  
-  export class RespuestaPreguntaDTO {
-    readonly numero: number; // Número de la pregunta correspondiente
-    readonly respuestaSeleccionada: string; // Respuesta del usuario (la opción elegida)
-  }
+  readonly user_id: string; // ID del usuario que responde el cuestionario
+  readonly cuestionario_id: string; // ID del cuestionario respondido
+  readonly respuestas: RespuestaPreguntaDTO[]; // Array de respuestas de las preguntas
+  readonly patente: string; // Patente del vehículo utilizado
+  readonly fecha_respuesta: Date; // Fecha en que se respondió el cuestionario
+  readonly geolocalizacion: {
+    latitud: number; // Latitud de la ubicación
+    longitud: number; // Longitud de la ubicación
+  }; // Geolocalización al momento de responder
+}
+
+export class RespuestaPreguntaDTO {
+  readonly numero: number; // Número de la pregunta correspondiente
+  readonly respuestaSeleccionada: string; // Respuesta del usuario (la opción elegida)
+}
